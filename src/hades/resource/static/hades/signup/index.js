1525660beaa89f73fbe102acb80ba441c42f19e3
KlnSdr <monsieurk1209@gmail.com>
2024-03-17 21:22:31 +0100

    add simple signup page and link signup on login

---
+++ src/hades/resource/static/hades/signup/index.js
@@ -0,0 +1,26 @@
+function doSignup() {
+    const username = document.getElementById('inputUsername').value;
+    const mail = document.getElementById('inputMail').value;
+    const password = document.getElementById('inputPassword').value;
+    const passwordRepeat = document.getElementById('inputPasswordRepeat').value;
+
+    fetch("/rest/users", {
+        method: "POST", headers: {
+            "Content-Type": "application/json"
+        }, body: JSON.stringify({
+            displayName: username, mail: mail, password: password, passwordRepeat: passwordRepeat
+        })
+    })
+        .then(response => {
+            if (response.status === 201) {
+                return response.json();
+            }
+            throw new Error("Signup failed");
+        })
+        .then(data => {
+            location.assign("/");
+        })
+        .catch(error => {
+            alert("Signup failed. Please try again.");
+        });
+}
