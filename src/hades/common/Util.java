5774d9af01533ffa7329618a392adeab0ba43eab
KlnSdr <monsieurk1209@gmail.com>
2025-03-31 18:00:52 +0200

    move convert from old to new json to extra package

--- src/hades/common/Util.java
+++ src/hades/common/Util.java
@@ -1,13 +1,7 @@
 package hades.common;
 
 import dobby.io.request.Request;
-import dobby.util.Json;
-import dobby.util.json.NewJson;
 import dobby.util.logging.Logger;
-
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.List;
 
 public class Util {
     private static final Logger LOGGER = new Logger(Util.class);
@@ -18,61 +12,6 @@ public class Util {
         }
 
         return "" + number;
-    }
-
-
-    public static NewJson convert(Json json) {
-        final NewJson newJson = new NewJson();
-
-        for (String valueMapName : new String[]{"stringData", "jsonData", "intData", "listData"}) {
-            Field field = null;
-            try {
-                field = Json.class.getDeclaredField(valueMapName);
-                field.setAccessible(true);
-
-                switch (valueMapName) {
-                    case "stringData":
-                        final HashMap<String, String> stringData = (HashMap<String, String>) field.get(json);
-                        for (String key : stringData.keySet()) {
-                            newJson.setString(key, stringData.get(key));
-                        }
-                        break;
-                    case "jsonData":
-                        final HashMap<String, Json> jsonData = (HashMap<String, Json>) field.get(json);
-                        for (String key : jsonData.keySet()) {
-                            newJson.setJson(key, convert(jsonData.get(key)));
-                        }
-                        break;
-                    case "intData":
-                        final HashMap<String, Integer> intData = (HashMap<String, Integer>) field.get(json);
-                        for (String key : intData.keySet()) {
-                            newJson.setInt(key, intData.get(key));
-                        }
-                        break;
-                    case "listData":
-                        final HashMap<String, List<Object>> listData = (HashMap<String, List<Object>>) field.get(json);
-                        for (String key : listData.keySet()) {
-                            final List<Object> list = listData.get(key);
-                            for (int i = 0; i < list.size(); i++) {
-                                final Object maybeJson = list.get(i);
-                                if (maybeJson instanceof Json) {
-                                    list.set(i, convert((Json) maybeJson));
-                                }
-                            }
-                            newJson.setList(key, list);
-                        }
-                        break;
-                    default:
-                        LOGGER.warn("Unknown valueMapName: " + valueMapName);
-                        break;
-                }
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                LOGGER.trace(e);
-                return null;
-            }
-        }
-
-        return newJson;
     }
 
     public static boolean requestHasHeader(Request request, String headerName) {
