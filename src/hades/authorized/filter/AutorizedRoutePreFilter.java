da9c28ef4222472484f69fe47447296492beccdc
KlnSdr <monsieurk1209@gmail.com>
2025-04-08 23:25:40 +0200

    remove uploaded files manually when client doesnt have permission

--- src/hades/authorized/filter/AutorizedRoutePreFilter.java
+++ src/hades/authorized/filter/AutorizedRoutePreFilter.java
@@ -3,6 +3,7 @@ package hades.authorized.filter;
 import dobby.filter.Filter;
 import dobby.filter.FilterType;
 import dobby.io.HttpContext;
+import dobby.io.request.RemoveTemporaryFilesPostFilter;
 import dobby.io.request.RequestTypes;
 import dobby.io.response.ResponseCodes;
 import dobby.util.json.NewJson;
@@ -44,6 +45,7 @@ public class AutorizedRoutePreFilter implements Filter {
 
         if (!UserService.getInstance().isLoggedIn(httpContext.getSession())) {
             httpContext.getResponse().setCode(ResponseCodes.FORBIDDEN);
+            new RemoveTemporaryFilesPostFilter().run(httpContext);
             return false;
         }
 
@@ -58,6 +60,7 @@ public class AutorizedRoutePreFilter implements Filter {
             payload.setString("error", "Invalid user id");
             httpContext.getResponse().setBody(payload);
 
+            new RemoveTemporaryFilesPostFilter().run(httpContext);
             return false;
         }
 
@@ -71,16 +74,15 @@ public class AutorizedRoutePreFilter implements Filter {
         final Group[] groups = GroupService.getInstance().findGroupsByUser(userId);
         for (Group group : groups) {
             for (Permission permission : group.getPermissions()) {
-                if (permission.getRoute().equalsIgnoreCase(matchingRoute)) {
-                    if (permission.hasPermission(requestMethod)) {
-                        return true;
-                    }
+                if (permission.getRoute().equalsIgnoreCase(matchingRoute) && permission.hasPermission(requestMethod)) {
+                    return true;
                 }
             }
         }
 
         if (!PermissionService.getInstance().hasPermission(userId, matchingRoute, requestMethod)) {
             httpContext.getResponse().setCode(ResponseCodes.FORBIDDEN);
+            new RemoveTemporaryFilesPostFilter().run(httpContext);
             return false;
         }
 
